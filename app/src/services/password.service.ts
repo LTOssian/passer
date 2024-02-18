40d5b4124be5b37705d8dfa92d02ccf2587eff4f
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-18 21:33:52 +0100

    feat: add jsdoc to service

--- app/src/services/password.service.ts
+++ app/src/services/password.service.ts
@@ -2,12 +2,17 @@ import { ICreatePassword, IPassword } from "../interfaces/password.model";
 import { AbstractPasswordService } from "./interfaces/password.abstract";
 
 class PasswordService extends AbstractPasswordService {
+  /**
+   * Generated a password object
+   * @param param0 contains name, length and constraints
+   * @returns name, strengh and password
+   */
   public static generatePassword({ name, length, constraints }: ICreatePassword): IPassword {
     const password = this.getPassword({ length, constraints });
 
     return {
       name,
-      strengh: this.getStrength({ length }),
+      strengh: this.getStrength({ length, constraints }),
       password,
     };
   }
