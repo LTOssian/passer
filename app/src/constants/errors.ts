e444a01bc51d6becdb853153aee89eb234566f1d
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-19 06:41:47 +0100

    feat: add more definitions in the errors enum and add icon width constant

--- app/src/constants/errors.ts
+++ app/src/constants/errors.ts
@@ -1,5 +1,9 @@
 enum ErrorsConstants {
   REQUIRED_INCLUDES = "Au moins une contrainte est requise.",
+  REQUIRE_TITLE = "Le titre est un champ requis.",
+  REQUIRE_PASSSWORD = "Le mot de passe est un champ requis.",
+  LT_PASSSWORD = "Le mot de passe doit avoir plus de 8 caractères.",
+  NOT_FOUND = "Le mot de passe n'a pas été trouvé.",
 }
 
 export default ErrorsConstants;
